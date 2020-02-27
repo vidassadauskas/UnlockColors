f458480684054e75fc63bf1930939ffc035f434b
Vidas Sadauskas <49838326+vidassadauskas@users.noreply.github.com>
2020-02-28 00:22:34 +0200

    Interim changes: ColorPie and MainWindow classes were refactored to allow create required color pies and have correct behaviour.

--- UnlockColors.py
+++ UnlockColors.py
@@ -5,6 +5,15 @@ Recipe: ?????   # TODO: Q: What is recipe
 """
 # TODO: try to rewrite the application using pygame.
 # TODO: try to rewrite the application using Pyglet.
+# TODO: what is the line limit according to PEP8? Why in Atom it is 80
+#       characters, in PyCharm - 120, in Spyder of Anaconda - ??? Which is
+#       right? How to fix it in IDE where it is not right to make them right
+#       and the same in all the IDE?
+# TODO: FIXME: move the main window to a secondary display ---> set to
+#       fullscreen ---> the window opens fullscreen in another (main) display.
+#       How to make the window remain in the same display?
+# FIXME:    Sort out when instance variables and when local variables should
+#           be declared and used and fix the code.
 
 # ======================================
 # Import required libraries and modules
@@ -63,7 +72,7 @@ class MainWindow:
                                         self.ConfigFileName + "."
         self.OkButtonLabel = "Gerai"                # Label of OK button
         # Colors used in the color wheel
-        self.Colors = ('red', 'orange', 'yellow', 'green', 'blue', 'purple')
+        self.Colors = ('yellow', 'green', 'blue', 'purple', 'red', 'orange')
         # Here we will keep color unlock codes
         self.ColorUnlockCodes = {
             self.Colors[0]: 99,
@@ -97,10 +106,7 @@ class MainWindow:
         # Assign the argument "master" to the instance variable "self.master"
         # to use in other functions of the class
         self.master = master
-        self.master.geometry("400x400")     # TODO: do we need to specify the
-        #                                           geometry of the window? Try
-        #                                           to comment out and see what
-        #                                           happens.
+        self.master.geometry("400x400")     # Default size of the main window
 
         # Set the main window title
         self.master.title(self.MainWindowTitle)
@@ -138,15 +144,51 @@ class MainWindow:
         self.master.bind("<F11>", self.enter_fullscreen)
         self.master.bind("<Escape>", self.quit_fullscreen)
 
+        # Create a canvas where pies will be placed.
+        self.Canvas = tk.Canvas(self.master)
         # Create a color pie instance
-        self.Pie = ColorPie(self.master, self.ColorUnlockCodes)
+        # Size of the pie compared to the containing window
+        self.PieRatio = 1.0
+        self.Pie = ColorPie(self.master, self.Canvas, self.ColorUnlockCodes,
+                            self.PieRatio)
+        # TODO: Make the size of the pie configurable in the options window
+        # Create the inner color pie
+        # Size of the inner pie compared to the outer pie
+        self.Pie2PieRatio = 0.3
+        # Size of the inner pie compared to the containing window
+        self.Pie0Ratio = self.PieRatio * self.Pie2PieRatio
+        self.Pie0 = ColorPie(self.master, self.Canvas, self.ColorUnlockCodes,
+                             self.Pie0Ratio)
 
     # Make the main window full screen
     def enter_fullscreen(self, *event):
-        self.master.attributes("-fullscreen", True)     # Enter full screen
         self.master.config(menu=self.empty_menu)        # Hide the menu bar
         self.action.place_forget()                      # Hide the button
+        self.master.attributes("-fullscreen", True)     # Enter full screen
+        # Extend the canvas to the length of the shortest side of the window
+        # Width of the calling window
+        width = self.master.winfo_width()
+        # Height of the calling window
+        height = self.master.winfo_height()
+        # Size of the canvas
+        canvas_size = int(min(width, height))
+        # Resize the canvas
+        self.Canvas.configure(width=canvas_size, height=canvas_size)
+        # Expand the pies to the full screen
+        self.Pie.resize(self.master, self.PieRatio)
+        self.Pie0.resize(self.master, self.Pie0Ratio)
         self.Pie.show()                                 # Show the color pie
+        self.Pie0.show()                                # Show the inner pie
+        # Some tasks in updating the display, such as resizing and redrawing
+        # widgets, are called idle tasks because they are usually deferred
+        # until the application has finished handling events and has gone back
+        # to the main loop to wait for new events. If you want to force the
+        # display to be updated before the application next idles, call the
+        # w.update_idletasks() method on any widget.
+        # TODO: Do we really need this?:
+        self.master.update_idletasks()
+        # FIXME:    When the window is fullscreen, the taskbar is visible an
+        #           top of the window
 
     # Exit fullscreen
     def quit_fullscreen(self, event):
@@ -154,7 +196,23 @@ class MainWindow:
         self.master.config(menu=self.menuBar)           # Restore the menu bar
         # Restore the button
         self.action.place(relx=0.5, rely=0.5, anchor='center')
+        # Resize the canvas to the length of the shortest side of the window
+        # Width of the calling window
+        # ? print("exit fullscreen", self.width)
+        width = self.master.winfo_width()
+        # Height of the calling window
+        height = self.master.winfo_height()
+        # Size of the canvas
+        canvas_size = int(min(width, height))
+        # Resize the canvas
+        self.Canvas.configure(width=canvas_size, height=canvas_size)
+        # Shrink the pies to the normal screen
+        self.Pie.resize(self.master, self.PieRatio)
+        self.Pie0.resize(self.master, self.Pie0Ratio)
         self.Pie.hide()                                 # Hide the color pie
+        self.Pie0.hide()                                # Hide the inner pie
+        # TODO: Do we really need this?:
+        self.master.update_idletasks()
 
     # Open a child window
     # While we need this function to open only the Options window, make it
@@ -202,13 +260,6 @@ class MainWindow:
                                  parent=self.master)
             self._quit()
 
-        # TODO: How to detect that the configuration file is present of absent?
-        # TODO: Config parsers do not guess data types of values in
-        #       configuration files, always storing them internally as strings.
-        #       This means that if you need other data types, you should
-        #       convert on your own:
-        #          >> > int(topsecret['Port'])
-
     # Exit GUI cleanly
     # TODO: Q: How to exit GUI cleanly?
     def _quit(self):
@@ -219,55 +270,103 @@ class MainWindow:
 
 # The color pie
 class ColorPie:
-    def __init__(self, master, color_unlock_codes):
+    def __init__(self, master, canvas, color_unlock_codes, ratio):
         # ======================================
         # Initialize instance variables
         # ======================================
         self.Wedges = []    # An array of wedges forming a color pie
-
         # Assign arguments to instance variables to use them in other functions
         # of the class
         self.master = master
+        self.canvas = canvas
         self.ColorUnlockCodes = color_unlock_codes
+        # TODO: what is the correct form of naming of instance variables:
+        #       camelCase, CamelCase or snake_case?
+        # ratio - a ratio of diameter of the pie and the shortest border
+        # (width or height, given as arguments to this function) of the
+        # calling window.
+        # Expected values are from 0.0 to 1.0. 1.0 means the pie fills all the
+        # window, i. e. 100%. If the value is greater than 1.0 then it is set
+        # to 1.0. If the value is less than 0.0 (i. e. if it is a negative
+        # value) then it is set to 0.0.
+        self.ratio = max(min(ratio, 1.0), 0.0)
+        # Width of the calling window
 
-        # Create a canvas for the color pie but do not show it yet
-        self.Canvas = tk.Canvas(self.master, width=0, height=0)
+        # Create a canvas for the color pie but do not show it yet.
+        length = len(self.ColorUnlockCodes)     # Total number of wedges
+        a = int('60', 16)       # Decimal code of the lightest shade of gray
+        b = int('A0', 16)       # Decimal code of the darkest shade of gray
+        self.outline_thickness = 10   # Thickness of active outline of a pie
         # Create a color pie as an array wedges but do not show them yet
         # Using enumerate since we need indexes (remember that dictionaries
         # don't have an order):
         for index, color in enumerate(self.ColorUnlockCodes):
+            # Shades of gray have to be distributed evenly from the lightest
+            # to the darkest.
+            rnd = format(int(a + index * (b - a) / length), 'x')
+            shade_of_gray = "#" + rnd * 3
             self.Wedges.append(
-                self.Canvas.create_arc(0, 0, self.master.winfo_screenwidth(),
-                                       self.master.winfo_screenheight(),
-                                       start=(index+0.25)*360/len(
-                                                        self.ColorUnlockCodes),
-                                       extent=360/len(self.ColorUnlockCodes),
-                                       fill=color, outline=color,
-                                       tags=color, style=tk.PIESLICE)
+                self.canvas.create_arc(0, 0, 1, 1,
+                                       start=(index+1)*360/length,
+                                       extent=360/length,
+                                       fill=shade_of_gray,
+                                       outline=shade_of_gray,
+                                       activeoutline=color,
+                                       activewidth=self.outline_thickness,
+                                       disabledfill=color,
+                                       tags=color, style=tk.PIESLICE,
+                                       state=tk.HIDDEN)
                                  )
+
+    # Resize the pie
+    def resize(self, master, ratio):
+        self.master = master
+        # ratio - a ratio of diameter of the pie and the shortest border
+        # (width or height, given as arguments to this function) of the
+        # calling window.
+        # Expected values are from 0.0 to 1.0. 1.0 means the pie fills all the
+        # window, i. e. 100%. If the value is greater than 1.0 then it is set
+        # to 1.0. If the value is less than 0.0 (i. e. if it is a negative
+        # value) then it is set to 0.0.
+        self.ratio = max(min(ratio, 1.0), 0.0)
+        # Size of the canvas
+        canvas_size = min(self.master.winfo_width(),
+                          self.master.winfo_height())
+        # A diameter of the pie
+        diam = int(canvas_size * self.ratio)
+
+        # Coordinates of the pie
+        # Left upper corner
+        xy0 = (canvas_size - diam) / 2 + self.outline_thickness
+        # Right lower corner
+        xy1 = (canvas_size + diam) / 2 - self.outline_thickness
+
+        # Resize wedges of the pie
+        # self.outline_thickness - leave some place between canvas border and
+        # the pie to display thick outline
+        for wedge in self.Wedges:
+            self.canvas.coords(wedge, xy0, xy0, xy1, xy1)
 
     # Display the canvas with the color pie
     def show(self):
         # TODO: what are option for place? could we use any of them?
-        self.Canvas.place(relx=0, rely=0)           # Make the canvas visible
-        # Extend the canvas to all the screen
-        self.Canvas.configure(width=self.master.winfo_width(),
-                              height=self.master.winfo_height())
+        # Make the canvas visible and centered
+        self.canvas.place(relx=0.5, rely=0.5, anchor='center')
         # Make the wedge visible
         for wedge in self.Wedges:
-            self.Canvas.itemconfigure(wedge, state=tk.NORMAL)
+            self.canvas.itemconfigure(wedge, state=tk.NORMAL)
 
     # Hide the canvas with the color pie
     def hide(self):
         # TODO: what are option for place? could we use any of them?
-        self.Canvas.place_forget()                  # Make the canvas invisible
+        self.canvas.place_forget()                  # Make the canvas invisible
         # Make the wedges invisible
         for wedge in self.Wedges:
-            self.Canvas.itemconfigure(wedge, state=tk.HIDDEN)
+            self.canvas.itemconfigure(wedge, state=tk.HIDDEN)
 
     def unlock_wedge(self, wedge_id, color):
         # FIXME: color?:
-        self.Canvas.itemconfigure(wedge_id, fill=color, outline=color)
+        self.canvas.itemconfigure(wedge_id, fill=color, outline=color)
 
 
 # Options window
